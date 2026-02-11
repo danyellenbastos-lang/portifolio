48e9fa0ebe5848f5fef9d69bb174be95255a2a4e
danyellenbastos-lang <danyellenbastos@gmail.com>
2026-02-11 10:05:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def contato():
 if __name__ == "__main__":
     port =  int(os.environ.get("PORT" , 5000))
     app.run(host="0.0.0.0" , port=port, debug=false)
+
 
